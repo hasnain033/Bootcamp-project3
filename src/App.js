c0d97d0164be1fb8d779014e58359b7917a17c7e
hasnain_ALI <hasnain6556@gmail.com>
2020-07-16 05:42:09 -0700

    first change

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>By hasnain Ali</p>
         <a
           className="App-link"
           href="https://reactjs.org"
